9c3e2fd78c115d13ffc581fbc3b227c67ec3255e
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-21 09:50:43 -0300

    feat: install and configure rxjs

--- packages/util-auth/src/mfe-util-auth.ts
+++ packages/util-auth/src/mfe-util-auth.ts
@@ -1,2 +1,54 @@
-// Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+import { BehaviorSubject } from 'rxjs'
+
+export const auth$ = new BehaviorSubject({
+  sessionToken: localStorage.getItem('@single-spa-app:sessionToken-1.0.0'),
+  error: false,
+  pending: false,
+})
+
+const GET_LOGGED_IN = (username: string, password: string) =>
+  new Promise((resolve, _) => {
+    auth$.next({
+      sessionToken: null,
+      error: false,
+      pending: true,
+    })
+
+    setTimeout(() => {
+      if (username === 'exampleuser' && password === 'examplepassword') {
+        const sessionToken = 'abc123def456'
+
+        localStorage.setItem('@single-spa-app:sessionToken-1.0.0', sessionToken)
+        
+        resolve({
+          sessionToken,
+          error: false,
+          pending: false,
+        })
+      } else {
+        resolve({
+          sessionToken: null,
+          error: 'Invalid user or password',
+          pending: false,
+        })
+      }
+    }, 2500)
+  })
+
+export function login(username: string, password: string) {
+  if (!auth$.value.pending) {
+    GET_LOGGED_IN(username, password).then((user: any) => {
+      auth$.next(user)
+    })
+  }
+}
+
+export function logout() {
+  localStorage.removeItem('@single-spa-app:sessionToken-1.0.0')
+
+  auth$.next({
+    sessionToken: null,
+    error: false,
+    pending: false,
+  })
+}
