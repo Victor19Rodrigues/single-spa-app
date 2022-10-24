68374f300be287bc66ed05e94904dbaec90e1bc3
Victor Rodrigues <vitor1908@gmail.com.br>
2022-10-24 07:10:32 -0300

    feat: create footer

---
+++ packages/app-footer/src/root.component.tsx
@@ -0,0 +1,9 @@
+import { ContainerFooter, Title } from './styles'
+
+export default function Root() {
+  return (
+    <ContainerFooter>
+      <Title>© 2022 Victor, Inc. All rights reserved.</Title>
+    </ContainerFooter>
+  )
+}
---
+++ packages/app-footer/src/styles.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const ContainerFooter = styled.footer`
+  padding: 4.8rem 4.8rem 1.7rem 4.8rem;
+  justify-content: center;
+  display: flex;
+  background-color: #121214;
+`
+export const Title= styled.span`
+  color: #c4c4cc;
+  font-size: 1rem;
+`
